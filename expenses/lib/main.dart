1db712cf1a0a0b31fd1c34e91cbae268f9476f66
juulia.sbianchi <juulia.sbianchi@gmail.com>
2023-08-18 12:09:59 -0300

    feat: Ajustando modo paisagem.

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -10,7 +10,6 @@ main() => runApp(ExpansesApp());
 class ExpansesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final ThemeData tema = ThemeData();
 
     return MaterialApp(
@@ -47,6 +46,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
@@ -88,19 +88,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    bool isLandScape = MediaQuery
+        .of(context)
+        .orientation == Orientation.landscape;
+
     final appBar = AppBar(
-      title: const Text('Despesas Pessoais'),
+      title: Text('Despesas Pessoais'),
       actions: [
         IconButton(
           icon: Icon(Icons.add),
           onPressed: () => _openTransactionFormModal(context),
         ),
+        if(isLandScape)
+          IconButton(
+          icon: Icon(_showChart ? Icons.list : Icons.bar_chart),
+          onPressed: () {
+            setState(() {
+              _showChart = !_showChart;
+            });
+          },
+        ),
       ],
     );
 
-    final availableHeight = MediaQuery.of(context).size.height -
+    final availableHeight = MediaQuery
+        .of(context)
+        .size
+        .height -
         appBar.preferredSize.height -
-        MediaQuery.of(context).padding.top;
+        MediaQuery
+            .of(context)
+            .padding
+            .top;
 
     return Scaffold(
       appBar: appBar,
@@ -108,18 +128,36 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-                height: availableHeight * 0.25,
-                child: Chart(_recentTransactions)),
-            Container(
-                height: availableHeight * 0.75                                                                                                                                                                                          ,
-                child: TransactionList(_transactions, _removeTransaction)),
+            if(isLandScape)
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     Text('Exibir Gráfico'),
+              //     Switch(
+              //       value: _showChart,
+              //       onChanged: (value) {
+              //         setState(() {
+              //           _showChart = value;
+              //         });
+              //       },
+              //     ),
+              //   ],
+              // ),
+            if (_showChart || !isLandScape)
+              Container(
+                height: availableHeight * (isLandScape ? 0.7 : 0.3),
+                child: Chart(_recentTransactions),
+              ),
+            if (!_showChart || !isLandScape)
+              Container(
+                  height: availableHeight * 0.75,
+                  child: TransactionList(_transactions, _removeTransaction)),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _openTransactionFormModal(context),
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
