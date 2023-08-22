f448232a7f94a743fd505e6dc0887ecd49b18bee
juulia.sbianchi <juulia.sbianchi@gmail.com>
2023-08-22 14:57:47 -0300

    feat: Adicionando tema, estilo e uma nova screen.

---
+++ meals/lib/components/category_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/category_meals_screen.dart';
+import '../models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+
+  final Category category;
+  const CategoryItem(this.category, {super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_){
+          return CategoryMealsScreen(category);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Colors.black,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(colors: [
+            category.color.withOpacity(0.4),
+            category.color,
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+      ),
+    );
+  }
+}
--- meals/lib/screens/categories_screen.dart
+++ meals/lib/screens/categories_screen.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/category_widget.dart';
+import 'package:meals/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200, //cada elemento vai ter uma extensão de no máximo 200px
-        childAspectRatio: 3 / 2, //proporção de cada elemento
-        crossAxisSpacing: 20,    // espaçamento no eixo cruzado
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vamos Cozinhar?'),
+        centerTitle: true,
       ),
-      children: const [],
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          //cada elemento vai ter uma extensão de no máximo 200px
+          childAspectRatio: 3 / 2,
+          //proporção de cada elemento
+          crossAxisSpacing: 20,
+          // espaçamento no eixo cruzado
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
