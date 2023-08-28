13a68958a782e15391d23ee78319926d2711eb45
juulia.sbianchi <juulia.sbianchi@gmail.com>
2023-08-28 16:31:11 -0300

    feat: Adicionando grid.

---
+++ shop/lib/models/product_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+
+class ProductList with ChangeNotifier{ // mixin
+
+  List<Product> _items = dummy_products;
+  List<Product> get items => [..._items]; // fazer um clone da lista
+
+  void addProduct(Product product){
+    _items.add(product);
+    notifyListeners();
+  }
+}
--- shop/lib/pages/products_overview_page.dart
+++ shop/lib/pages/products_overview_page.dart
@@ -1,37 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_item.dart';
 import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product_list.dart';
 
+import '../components/product_grid.dart';
 import '../models/product.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummy_products;
-
   ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: const Text(
-            'Minha Loja',
-          ),
-        ),
+        title: const Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        // mostra os produtos que estão na lista, tamanho da lista
-        itemCount: loadedProducts.length,
-        //estrutura de grid view com dois elementos cada coluna
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2, // 1.5, relação entre altura e largura
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-      ),
+      body: ProductGrid(),
     );
   }
 }
