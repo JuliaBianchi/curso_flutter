0c50d566d35c333b2b150e1357730202c6938d1e
juulia.sbianchi <juulia.sbianchi@gmail.com>
2023-08-31 16:50:43 -0300

    feat: Iniciando com requisições post, exibindo indicador de progresso e tratamento de erro.

--- shop/lib/models/product_list.dart
+++ shop/lib/models/product_list.dart
@@ -1,11 +1,13 @@
+import 'dart:convert';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
   // mixin
+  final _baseUrl = 'https://shop-flutter-ced3e-default-rtdb.firebaseio.com';
 
   final List<Product> _items = dummy_products;
 
@@ -18,9 +20,8 @@ class ProductList with ChangeNotifier {
     return _items.length;
   }
 
-  void saveProduct(Map<String, Object> data) {
-
-    final hasId = data['id'] !=null;
+  Future<void> saveProduct(Map<String, Object> data) {
+    final hasId = data['id'] != null;
 
     final product = Product(
       id: hasId ? data['id'] as String : Random().nextDouble().toString(),
@@ -30,31 +31,58 @@ class ProductList with ChangeNotifier {
       imageUrl: data['imageUrl'] as String,
     );
 
-    if(hasId){
-      updateProduct(product);
-    }else{
-      addProduct(product);
+    if (hasId) {
+      return updateProduct(product);
+    } else {
+      return addProduct(product);
     }
   }
 
-  void addProduct(Product product) {
-    _items.add(product);
-    notifyListeners();
+  Future<void> addProduct(Product product) {
+    final future = http.post(
+      Uri.parse('$_baseUrl/products.json'),
+      body: jsonEncode(
+        {
+          "name": product.name,
+          "description": product.description,
+          "price": product.price,
+          "imageUrl": product.imageUrl,
+          "isFavorite": product.isFavorite,
+        },
+      ),
+    );
+    // executar algo depois que foi no firebase e voltou
+    return future.then<void>((response) {
+      final id = jsonDecode(response.body)['name'];
+      //print(jsonDecode(response.body)); - ver o que veio no corpo da resposta
+      _items.add(
+        Product(
+            id: id,
+            name: product.name,
+            description: product.description,
+            price: product.price,
+            imageUrl: product.imageUrl,
+        ),
+      );
+      notifyListeners();
+    });
   }
 
-  void updateProduct(Product product){
+  Future<void> updateProduct(Product product) {
     int index = _items.indexWhere((p) => p.id == product.id);
 
-    if(index >= 0){
+    if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
+
+    return Future.value();
   }
 
-  void deleteProduct(Product product){
+  void deleteProduct(Product product) {
     int index = _items.indexWhere((p) => p.id == product.id);
 
-    if(index >= 0){
+    if (index >= 0) {
       _items.removeWhere((p) => p.id == product.id);
       notifyListeners();
     }
