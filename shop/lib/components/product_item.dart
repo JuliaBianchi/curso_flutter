796524f9c716e093a2798f1520ba2cfd39d11ca8
juulia.sbianchi <juulia.sbianchi@gmail.com>
2023-08-31 11:04:42 -0300

    feat: Fazendo formulário.

--- shop/lib/components/product_item.dart
+++ shop/lib/components/product_item.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 
 import '../models/product.dart';
+import '../utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
   final Product product;
@@ -21,13 +24,42 @@ class ProductItem extends StatelessWidget {
             IconButton(
               icon: const Icon(Icons.edit),
               color: Theme.of(context).primaryColor,
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.product_form,
+                  arguments: product,
+                );
+              },
             ),
-
             IconButton(
               icon: const Icon(Icons.delete),
               color: Theme.of(context).colorScheme.error,
-              onPressed: () {},
+              onPressed: () {
+                showDialog<bool>(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    content: Text('Quer excluir esse produto?'),
+                    actions: [
+                      TextButton(
+                        child: Text('Não'),
+                        onPressed: () =>
+                            Navigator.of(ctx).pop(false), // fechar o pop up
+                      ),
+                      TextButton(
+                        child: Text('Sim'),
+                        onPressed: () => Navigator.of(ctx).pop(true),
+                      ),
+                    ],
+                  ),
+                ).then((value) {
+                  if (value ?? false) {
+                    Provider.of<ProductList>(
+                      context,
+                      listen: false,
+                    ).deleteProduct(product);
+                  }
+                });
+              },
             ),
           ],
         ),
--- shop/lib/models/product_list.dart
+++ shop/lib/models/product_list.dart
@@ -1,12 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
-class ProductList with ChangeNotifier{ // mixin
+class ProductList with ChangeNotifier {
+  // mixin
 
   final List<Product> _items = dummy_products;
 
-  List<Product> get items =>  [..._items]; // fazer um clone da lista
+  List<Product> get items => [..._items]; // fazer um clone da lista
 
   List<Product> get favoriteItems =>
       _items.where((prod) => prod.isFavorite).toList();
@@ -15,8 +18,45 @@ class ProductList with ChangeNotifier{ // mixin
     return _items.length;
   }
 
-  void addProduct(Product product){
+  void saveProduct(Map<String, Object> data) {
+
+    final hasId = data['id'] !=null;
+
+    final product = Product(
+      id: hasId ? data['id'] as String : Random().nextDouble().toString(),
+      name: data['name'] as String,
+      description: data['description'] as String,
+      price: data['price'] as double,
+      imageUrl: data['imageUrl'] as String,
+    );
+
+    if(hasId){
+      updateProduct(product);
+    }else{
+      addProduct(product);
+    }
+  }
+
+  void addProduct(Product product) {
     _items.add(product);
     notifyListeners();
+  }
+
+  void updateProduct(Product product){
+    int index = _items.indexWhere((p) => p.id == product.id);
+
+    if(index >= 0){
+      _items[index] = product;
+      notifyListeners();
+    }
+  }
+
+  void deleteProduct(Product product){
+    int index = _items.indexWhere((p) => p.id == product.id);
+
+    if(index >= 0){
+      _items.removeWhere((p) => p.id == product.id);
+      notifyListeners();
+    }
   }
 }
