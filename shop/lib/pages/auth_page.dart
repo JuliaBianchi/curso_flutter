32b9eedef379420475b95d0033900ca20ada7b82
juulia.sbianchi <juulia.sbianchi@gmail.com>
2023-09-04 09:46:02 -0300

    feat: AuthForm e validação dos campos.

--- shop/lib/pages/auth_page.dart
+++ shop/lib/pages/auth_page.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+import '../components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -27,22 +31,15 @@ class AuthPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  child: Text(
-                    'Minha Loja',
-                    style: TextStyle(
-                      fontSize: 45,
-                      fontFamily: 'Anton',
-                      color: Colors.white,
-                    ),
-                  ),
                   padding: const EdgeInsets.symmetric(
                     horizontal: 70,
                     vertical: 10,
                   ),
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     color: Colors.deepOrange.shade900,
-                    boxShadow: [
+                    boxShadow: const [
                       BoxShadow(
                         blurRadius: 8,
                         color: Colors.black38,
@@ -50,10 +47,20 @@ class AuthPage extends StatelessWidget {
                       ),
                     ]
                   ),
+                  child: const Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: 'Anton',
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
+                AuthForm(),
 
               ],
             ),
+
           )
         ],
       ),
