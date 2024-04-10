d49a92589c5044d72b5f5180fad1fe1e4325a286
Julia Spader Bianchi <julia.bianchi@soluct.com.br>
2024-04-10 13:38:40 -0300

    Adicionando store e funções de incrementar e decrementar na página.

--- pomodoro_app_flutter/lib/pages/pomodoro_page.dart
+++ pomodoro_app_flutter/lib/pages/pomodoro_page.dart
@@ -1,14 +1,19 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:pomodoro_app_flutter/components/input_time_component.dart';
 import 'package:pomodoro_app_flutter/components/stopwatch_component.dart';
+import 'package:pomodoro_app_flutter/store/pomodoro_store.dart';
+import 'package:provider/provider.dart';
 
 class PomodoroPage extends StatelessWidget {
   const PomodoroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+
+    final store = Provider.of<PomodoroStore>(context);
+
+    return Scaffold(
       body: Column(
         // esticar no eixo cruzado,
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -16,12 +21,14 @@ class PomodoroPage extends StatelessWidget {
           Expanded(child: StopwatchComponent()),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 40),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                InputTimeComponent(title: 'Trabalho', value: 25),
-                InputTimeComponent(title: 'Descanso', value: 5),
-              ],
+            child: Observer(
+              builder: (_) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  InputTimeComponent(title: 'Trabalho', value: store.workTime, increment: store.incrementWorkTime, decrement: store.decrementWorkTime),
+                  InputTimeComponent(title: 'Descanso', value: store.timeOff, increment: store.incrementTimeOff, decrement: store.decrementTimeOff),
+                ],
+              ),
             ),
           ),
         ],
