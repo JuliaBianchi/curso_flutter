aaecc7787a6ee3b6b6017e9badc66af08e991b58
Julia Spader Bianchi <julia.bianchi@soluct.com.br>
2024-04-11 08:37:29 -0300

    Mudando cores dos textos.

--- pomodoro_app_flutter/lib/components/stopwatch_component.dart
+++ pomodoro_app_flutter/lib/components/stopwatch_component.dart
@@ -1,45 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:pomodoro_app_flutter/components/stopwatch_button_component.dart';
+import 'package:pomodoro_app_flutter/store/pomodoro_store.dart';
+import 'package:provider/provider.dart';
 
 class StopwatchComponent extends StatelessWidget {
   const StopwatchComponent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.redAccent.shade200,
-      child: const Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Hora de Trabalhar',
-            style: TextStyle(fontSize: 40, color: Colors.white),
-          ),
-          Text(
-            '25:00',
-            style: TextStyle(fontSize: 100, color: Colors.white),
-          ),
-          Row(
+    final store = Provider.of<PomodoroStore>(context);
+
+    return Observer(
+      builder: (_) {
+        return Container(
+          color: store.isWorking() ? Colors.red.shade400 : Colors.green.shade400,
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Padding(
-                padding:  EdgeInsets.all(10.0),
-                child: StopwatchButtonComponent(text: 'Iniciar', icon: Icons.play_arrow),
+               Padding(
+                padding: const EdgeInsets.only(top: 5, bottom: 20, left: 10, right: 10),
+                child: Text('POMODORO TIMER', style: TextStyle(fontSize: 35, color: store.isWorking() ? const Color(0xFF970C10) : const Color(0xFF116530), fontWeight: FontWeight.bold)),
               ),
               Padding(
-                padding:  EdgeInsets.all(10.0),
-                child: StopwatchButtonComponent(text: 'Parar', icon: Icons.stop),
+                padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+                child: Text(
+                  store.isWorking() ? 'FOCUS' : 'BREAK',
+                  style: const TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
+                ),
               ),
-              Padding(
-                padding:  EdgeInsets.all(10.0),
-                child: StopwatchButtonComponent(text: 'Reiniciar', icon: Icons.refresh),
+              Text(
+                '${store.minutes.toString().padLeft(2, '0')}:${store.seconds.toString().padLeft(2, '0')}',
+                style: const TextStyle(fontSize: 100, color: Colors.white),
               ),
-
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (!store.started)
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: StopwatchButtonComponent(
+                          text: 'Start',
+                          icon: Icons.play_arrow,
+                          click: store.start,
+                      ),
+                    ),
+                  if (store.started)
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: StopwatchButtonComponent(
+                          text: 'Stop', icon: Icons.stop, click: store.stop,
+                      ),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: StopwatchButtonComponent(
+                        text: 'Restart',
+                        icon: Icons.refresh,
+                        click: store.refresh,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
