4b6f83644e9b79bb8343d6a81e7c41b285f21d80
Julia Spader Bianchi <julia.bianchi@soluct.com.br>
2024-04-11 08:36:27 -0300

    Adicionando função de click.

--- pomodoro_app_flutter/lib/components/stopwatch_button_component.dart
+++ pomodoro_app_flutter/lib/components/stopwatch_button_component.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class StopwatchButtonComponent extends StatelessWidget {
   final String text;
   final IconData icon;
+  final void Function()? click;
 
   const StopwatchButtonComponent({
     super.key,
     required this.text,
-    required this.icon
+    required this.icon,
+    this.click
   });
 
   @override
@@ -16,14 +18,14 @@ class StopwatchButtonComponent extends StatelessWidget {
       style: ElevatedButton.styleFrom(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       ),
-      onPressed: () {},
+      onPressed: click,
       child: Row(
         children: [
           Padding(
             padding: const EdgeInsets.only(right: 10.0),
             child: Icon(icon, color: Colors.black),
           ),
-          Text(text, style: const TextStyle(color: Colors.black),),
+          Text(text, style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
         ],
       ),
     );
